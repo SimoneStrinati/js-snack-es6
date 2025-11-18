5d93910d4bb2f6a0724b080335a8e9d21939c693
simonestrinati <simone.strinati@outlook.it>
2025-11-18 11:17:38 +0100

    Snack 1 terminato

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
-const racingBike = [
+const bicycle = [
 
-    { name: "Amflow", weight: 15},
-    { name: "Bergamont", weight: 12},
+    { name: "Amflow", weight: 15 },
+    { name: "Bergamont", weight: 12 },
     { name: "Bianchi", weight: 8 },
-    { name: "Bold", weight: 13},
+    { name: "Bold", weight: 13 },
 
 ];
 
-for (let i = 0; i < racingBike.length; i++) {
-    const bike = racingBike[i];
+// for (let i = 0; i < bicycle.length; i++) {
+//     const bike = bicycle[i];
 
-    console.log(`La ${bike.name} pesa ${bike.weight}`);
-    
+//     console.log(`La ${bike.name} pesa ${bike.weight}`);
+
+// }
+
+
+let lightBike = bicycle[0];
+
+for (let i = 0; i < bicycle.length; i++) {
+    const bike = bicycle[i];
+
+    if (bike.weight < lightBike.weight) {
+        lightBike = bike;
+    }
+
 }
+
+  console.log("La bici più leggera è:", lightBike)
+
+
 
